17bdb049cfb7610b9c3e1b7b6f576a271468980c
Ibrahiim-Ahmed <ibrahimahmed3647@gmail.com>
2023-05-22 11:49:51 +0500

    Finished the app

--- lib/main.dart
+++ lib/main.dart
@@ -5,113 +5,281 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData.dark(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-
-          ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData.dark(),
+      home: const MyHomePage(title: ''),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  double billAmount = 0.0;
+  double tipPercentage = 15.0;
+  int splitCount = 1;
 
-  void _incrementCounter() {
+  double calculateTip() {
+    return billAmount * (tipPercentage / 100);
+  }
+
+  double calculateTotalBill() {
+    return billAmount + calculateTip();
+  }
+
+  double calculateBillPerPerson() {
+    return calculateTotalBill() / splitCount;
+  }
+
+  double calculateTipPerPerson() {
+    return calculateTip() / splitCount;
+  }
+
+  void incrementSplitCount() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      splitCount++;
     });
+  }
+
+  void decrementSplitCount() {
+    if (splitCount > 1) {
+      setState(() {
+        splitCount--;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: [const DrawerHeader(child: Text("Tip"))],
-          ),
-        ),
-        body: Center(
-          child: Card(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                const ListTile(
-                  leading: Icon(Icons.album),
-                  title: Text('The Enchanted Nightingale'),
-                  subtitle:
-                      Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+      appBar: AppBar(
+        backgroundColor: Colors.grey[850],
+      ),
+      drawer: Drawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[700],
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    TextButton(
-                      child: const Text('BUY TICKETS'),
-                      onPressed: () {/* ... */},
+                    Text(
+                      "Per Person",
+                      style: const TextStyle(
+                        color: Colors.pinkAccent,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    const SizedBox(width: 8),
-                    TextButton(
-                      child: const Text('LISTEN'),
-                      onPressed: () {/* ... */},
+                    Text(
+                      '\$${calculateTotalBill().toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    const SizedBox(width: 8),
+                    const SizedBox(height: 12.0),
+                    Text(
+                      "Sub Total",
+                      style: const TextStyle(
+                        color: Colors.pinkAccent,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '\$${calculateBillPerPerson().toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 12.0),
+                    Text(
+                      "Per Person",
+                      style: const TextStyle(
+                        color: Colors.pinkAccent,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '\$${calculateTipPerPerson().toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 40.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      tipPercentage = 10.0;
+                    });
+                  },
+                  child: const Text('10%'),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(8.0),
+                    primary: tipPercentage == 10.0
+                        ? Colors.pinkAccent
+                        : Colors
+                            .grey, // Change the button color to dark pink if selected, otherwise grey
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      tipPercentage = 15.0;
+                    });
+                  },
+                  child: const Text('15%'),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(8.0),
+                    primary: tipPercentage == 15.0
+                        ? Colors.pinkAccent
+                        : Colors
+                            .grey, // Change the button color to dark pink if selected, otherwise grey
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      tipPercentage = 20.0;
+                    });
+                  },
+                  child: const Text('20%'),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(8.0),
+                    primary: tipPercentage == 20.0
+                        ? Colors.pinkAccent
+                        : Colors
+                            .grey, // Change the button color to dark pink if selected, otherwise grey
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('Custom Tip Percentage'),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              TextField(
+                                onChanged: (value) {
+                                  setState(() {
+                                    tipPercentage =
+                                        double.tryParse(value) ?? 0.0;
+                                  });
+                                },
+                                keyboardType: TextInputType.number,
+                                decoration: const InputDecoration(
+                                  labelText: 'Enter tip percentage',
+                                ),
+                              ),
+                            ],
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Cancel'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Save'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: const Text('Custom'),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(8.0),
+                    primary: Colors.grey, // Change the button color to grey
+                  ),
                 ),
               ],
             ),
-          ),
-        ));
-    // Center is a layout widget. It takes a single child and positions it
-    // in the middle of the parent.
+            const SizedBox(height: 40.0),
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  billAmount = double.tryParse(value) ?? 0.0;
+                });
+              },
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Enter bill amount',
+              ),
+            ),
+            const SizedBox(height: 40.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: decrementSplitCount,
+                  child: const Icon(Icons.remove),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(8.0),
+                    shape: const CircleBorder(),
+                    primary: Colors.pink,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    splitCount.toString(),
+                    style: const TextStyle(fontSize: 24.0),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: incrementSplitCount,
+                  child: const Icon(Icons.add),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(8.0),
+                    shape: const CircleBorder(),
+                    primary: Colors.pink,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
